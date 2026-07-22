1bb5dff80eb2603425b9cad3652d78b238663f2b
Jan Wennrich <jan.wennrich@pcsg.de>
2026-07-22 17:09:47 +0200

    test: add tests
    Related: quiqqer/core#1472

--- tests/QUI/Log/Monolog/QuiqqerFileFormatterTest.php
+++ tests/QUI/Log/Monolog/QuiqqerFileFormatterTest.php
@@ -11,6 +11,22 @@ use RuntimeException;
 
 class QuiqqerFileFormatterTest extends TestCase
 {
+    public function testRecordWithoutContextOrExtraStaysOnOneLine(): void
+    {
+        $Formatter = new QuiqqerFileFormatter();
+        $record = new LogRecord(
+            datetime: new DateTimeImmutable('2030-01-02 23:59:59.123456+14:00'),
+            channel: 'test',
+            level: Level::Info,
+            message: 'Everything is fine'
+        );
+
+        self::assertSame(
+            '[2030-01-02T23:59:59.123456+14:00] INFO: Everything is fine' . PHP_EOL,
+            $Formatter->format($record)
+        );
+    }
+
     public function testCompleteRecordIsFormatted(): void
     {
         $Formatter = new QuiqqerFileFormatter();
@@ -52,9 +68,19 @@ LOG . PHP_EOL,
             context: ['exception' => new RuntimeException('Something failed', 42)]
         );
 
-        self::assertStringContainsString(
-            '"class": "RuntimeException"',
-            $Formatter->format($record)
+        $formattedRecord = $Formatter->format($record);
+
+        self::assertSame(
+            [
+                'classIsPresent' => true,
+                'messageIsPresent' => true,
+                'codeIsPresent' => true
+            ],
+            [
+                'classIsPresent' => str_contains($formattedRecord, '"class": "RuntimeException"'),
+                'messageIsPresent' => str_contains($formattedRecord, '"message": "Something failed"'),
+                'codeIsPresent' => str_contains($formattedRecord, '"code": 42')
+            ]
         );
     }
 }
--- tests/QUI/Log/Monolog/QuiqqerMetadataProcessorTest.php
+++ tests/QUI/Log/Monolog/QuiqqerMetadataProcessorTest.php
@@ -10,6 +10,28 @@ use QUI\Log\Monolog\QuiqqerMetadataProcessor;
 
 class QuiqqerMetadataProcessorTest extends TestCase
 {
+    /**
+     * @var array<string, mixed>
+     */
+    private array $OriginalServer;
+
+    /**
+     * @var array<string, mixed>
+     */
+    private array $OriginalRequest;
+
+    protected function setUp(): void
+    {
+        $this->OriginalServer = $_SERVER;
+        $this->OriginalRequest = $_REQUEST;
+    }
+
+    protected function tearDown(): void
+    {
+        $_SERVER = $this->OriginalServer;
+        $_REQUEST = $this->OriginalRequest;
+    }
+
     public function testFilenameIsMovedFromContextToExtra(): void
     {
         $Processor = new QuiqqerMetadataProcessor();
@@ -36,5 +58,70 @@ class QuiqqerMetadataProcessorTest extends TestCase
                 'correlationId' => $processedRecord->extra['correlationId']
             ]
         );
+    }
+
+    public function testExistingQuiqqerMetadataIsPreserved(): void
+    {
+        $Processor = new QuiqqerMetadataProcessor();
+        $record = new LogRecord(
+            datetime: new DateTimeImmutable(),
+            channel: 'test',
+            level: Level::Warning,
+            message: 'test',
+            context: ['filename' => 'auth'],
+            extra: ['quiqqer' => ['correlationId' => '123']]
+        );
+
+        $processedRecord = $Processor($record);
+
+        self::assertSame('123', $processedRecord->extra['quiqqer']['correlationId']);
+        self::assertSame('auth', $processedRecord->extra['quiqqer']['filename']);
+    }
+
+    public function testRequestMetadataIsAddedWithoutChangingApplicationContext(): void
+    {
+        $_SERVER['REQUEST_URI'] = '/admin/?test=1';
+        $_REQUEST['quiqqerBundle'] = 'package_ajax_method';
+
+        $Processor = new QuiqqerMetadataProcessor();
+        $record = new LogRecord(
+            datetime: new DateTimeImmutable(),
+            channel: 'test',
+            level: Level::Warning,
+            message: 'test',
+            context: ['operation' => 'login']
+        );
+
+        $processedRecord = $Processor($record);
+
+        self::assertSame(
+            [
+                'context' => ['operation' => 'login'],
+                'request' => HOST . '/admin/?test=1',
+                'ajaxBundler' => 'package_ajax_method'
+            ],
+            [
+                'context' => $processedRecord->context,
+                'request' => $processedRecord->extra['quiqqer']['request'],
+                'ajaxBundler' => $processedRecord->extra['quiqqer']['ajaxBundler']
+            ]
+        );
+    }
+
+    public function testInvalidExistingQuiqqerMetadataIsReplaced(): void
+    {
+        $Processor = new QuiqqerMetadataProcessor();
+        $record = new LogRecord(
+            datetime: new DateTimeImmutable(),
+            channel: 'test',
+            level: Level::Warning,
+            message: 'test',
+            context: ['filename' => 'auth'],
+            extra: ['quiqqer' => 'invalid']
+        );
+
+        $processedRecord = $Processor($record);
+
+        self::assertSame(['filename' => 'auth'], $processedRecord->extra['quiqqer']);
     }
 }
